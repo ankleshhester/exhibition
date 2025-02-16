4f1cc62b302b7018fb41977859dd7fdff78c70d9
ankleshhester <anklesh.chauhan@hester.in>
2025-02-17 00:56:24 +0530

    q

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="relative w-full h-full py-40 min-h-screen">
+<section class="relative w-full h-full py-3 min-h-screen">
     <div class="container mx-auto px-4 h-full">
         <div class="flex content-center items-center justify-center h-full">
             <div class="w-full lg:w-4/12 px-4">
-                <div class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
+                <div class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-white border-0">
                     <div class="rounded-t mb-0 px-6 py-6">
                         <div class="text-center mb-3">
                             <h6 class="text-blueGray-500 text-sm font-bold">
-                                {{ __('global.login') }}
+                                Admin Login
                             </h6>
                         </div>
                         <hr class="mt-6 border-b-1 border-blueGray-300" />
