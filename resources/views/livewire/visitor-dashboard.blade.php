4fe78316f3c6dc7602591f763c34742e027606fa
ankleshhester <anklesh.chauhan@hester.in>
2025-03-09 00:08:05 +0530

    111

--- resources/views/livewire/visitor-dashboard.blade.php
+++ resources/views/livewire/visitor-dashboard.blade.php
@@ -3,7 +3,7 @@
 <div class="row">
     <div class="card bg-white">
         <div class="card-header border-b border-blueGray-200" style="border-radius:0 !important">
-            <div class="card-header-container" style="text-align: left !important">
+            <div class="" style="text-align: center !important">
                 <h3 class="">
                     Thank you {{ $visitor_name }}, <br>for visiting us at VIV Asia, 2025
                 </h3>
